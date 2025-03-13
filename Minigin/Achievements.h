265f3f2c3ddb50221525dd2d26973de3b4e15577
introsent <161451906+introsent@users.noreply.github.com>
2025-03-13 15:22:53 +0100

    feat: add Achievements.h

---
+++ Minigin/Achievements.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define _ACH_ID(id, name) { id, #id, name, "", false, 0 }
+
+struct Achievement_t
+{
+    int m_eAchievementID;        
+    const char* m_pchAchievementID;
+    char m_rgchName[128];           
+    char m_rgchDescription[256];  
+    bool m_bAchieved;             
+    int m_iIconImage;               
+
