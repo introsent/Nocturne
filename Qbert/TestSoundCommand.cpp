98abb0ed8b2ac710d0d41518e46f6d7ed7752257
introsent <161451906+introsent@users.noreply.github.com>
2025-04-25 16:14:18 +0200

    feat: test threading

--- Qbert/TestSoundCommand.cpp
+++ Qbert/TestSoundCommand.cpp
@@ -1,13 +1,21 @@
 #include "TestSoundCommand.h"
+//#include <iostream>
 #include "SoundServiceLocator.h"
 
 using namespace dae;
 
 void TestSoundCommand::Execute()
 {
-	auto* soundService = SoundServiceLocator::GetService();
-	if (soundService) {
-		soundService->PlaySound("test");  // Works now
+	if (auto* soundService = SoundServiceLocator::GetService()) {
+		soundService->PlaySound("test");  
 	}
+
+	//	to test threading only
+	//td::cout << "Main thread continues after PlaySound...\n";
+	//or (int i = 0; i < 10000000; ++i) {
+	//	volatile double result = sqrt(i) * tan(i);
+	//	std::cout << result << "\n"; 
+	//
+	//td::cout << "Main thread finished heavy work\n";
 }
 
